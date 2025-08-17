a6b0ced2d1b76b77cf7b43426d505874ffb70fd0
Equinox-jpeg <aswg8000@gmail.com>
2025-08-17 22:10:01 +0800

    Delete Book-Tune directory

--- Book-Tune/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "book_tune";
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) { die("DB connection failed: " . $conn->connect_error); }
-?>
--- Book-Tune/premium_ids.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Server-side source of truth for which books are premium-only
-$PREMIUM_BOOK_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-?>
